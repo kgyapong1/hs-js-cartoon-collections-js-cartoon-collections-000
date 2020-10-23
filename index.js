a77fe7487834621082b3f1ee82d1555f8108f08f
Kwaku Gyapong <kwakugyapong219@gmail.com>
2020-10-23 00:33:46 +0000

    Done.

--- index.js
+++ index.js
@@ -1,11 +1,55 @@
-function dwarfRollCall(dwarves) {
+function dwarfName(foo) {
+
+	// Defining variable
+	var finalString;
+  
+  // Loop through the array, assigning/adding each name to finalString
+	for(var i=0;i<foo.length;i++){
+  	finalString =+ (i+1) + '. ' + foo[i] + ' ';
+  }
+  
+  // Return final string with all names
+  return finalString;
+  
 }
 
-function summonCaptainPlanet(planeteerCalls){
+var calls = ["earth", "wind", "fire", "water"];
+
+function planetCalls(foo) {
+
+	// Create storage variables
+ 	var capsCall;
+  var capsArray = [];
+  
+  // Begin loop
+	for(var i=0;i<foo.length;i++) {
+    capsCall = foo[i].toUpperCase() + '!';
+    capsArray.push(capsCall);
+  }
+  
+  return capsArray;
 }
 
-function longPlaneteerCalls(words) {
+planetCalls(calls);
+
+
+
+
+
+function longPlaneteerCalls(foo) {
+
+var i;
+var result;
+  
+	for (i = 0; i < foo.length; i++) {
+    if(foo[i].length > 4) {
+    	result = true
+    } else {
+    	result = false
+    }
+  }
+  
+  return result
 }
 
-function findTheCheese (foods) {
-}
+
